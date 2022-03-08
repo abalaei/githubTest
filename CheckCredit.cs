cb2619f6455d451f43d0725007b50ba829321eba
a.balaei <a.t.balaei@gmail.com>
2022-03-08 12:25:33 +1100

    Create CheckCredit.cs
    new c# code

---
+++ CheckCredit.cs
@@ -0,0 +1,16 @@
+using System;
+using static System.Console;
+class CheckCredit
+{
+   static void Main()
+   {
+      double price;
+      const double LIMIT = 8000;
+      Write("Enter price >> ");
+      price = Convert.ToDouble(ReadLine());
+      if(price > LIMIT)
+         WriteLine("You have exceeded the credit limit of ${0}", LIMIT);
+       else
+          WriteLine("Approved");
+   }
+}
